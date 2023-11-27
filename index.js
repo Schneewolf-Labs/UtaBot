649d59bc51c4ee606e5bee9573e36b71a8969a08
Nicholas Beerbower <nbeerbower@gmail.com>
2023-11-27 14:56:30 -0500

    add more params

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 const token = process.env.DISCORD_BOT_TOKEN;
-const apiEndpoint = 'http://127.0.0.1:5000/generate';
+const apiEndpoint = 'http://127.0.0.1:5002/generate';
 
 let queue = [];
 
@@ -18,7 +18,12 @@ client.once('ready', async () => {
 			.setName('generate')
 			.setDescription('Generate music from a prompt')
 			.addStringOption(option => option.setName('prompt').setDescription('The prompt for the music').setRequired(true))
-			.addIntegerOption(option => option.setName('duration').setDescription('The duration of the music in seconds').setRequired(false)),
+			.addIntegerOption(option => option.setName('duration').setDescription('The duration of the music in seconds').setMinValue(5).setMaxValue(300))
+			.addNumberOption(option => option.setName('temperature').setDescription('Softmax temperature for generation').setMinValue(0.0).setMaxValue(1.0))
+			.addIntegerOption(option => option.setName('top_k').setDescription('Top K value for sampling').setMinValue(1).setMaxValue(500))
+      .addNumberOption(option => option.setName('top_p').setDescription('Top P value for sampling').setMinValue(0.0).setMaxValue(1.0))
+			.addNumberOption(option => option.setName('cfg_coef').setDescription('Coefficient for classifier free guidance').setMinValue(0.0).setMaxValue(10.0))
+			.addNumberOption(option => option.setName('extend_stride').setDescription('Stride length for extended generation').setMinValue(1).setMaxValue(30))
 	].map(command => command.toJSON());
 
 	const rest = new REST().setToken(token);
@@ -53,9 +58,16 @@ client.on('interactionCreate', async interaction => {
 
 	if (commandName === 'generate') {
 		const prompt = options.getString('prompt');
-		const duration = Math.min(300, options.getInteger('duration') || 90);
+		const params = {
+			duration: options.getInteger('duration') || 60,
+			temperature: options.getNumber('temperature'),
+			top_k: options.getInteger('top_k'),
+			top_p: options.getNumber('top_p'),
+			cfg_coef: options.getNumber('cfg_coef'),
+			extend_stride: options.getInteger('extend_stride')
+		};
 		console.log(`Received prompt: ${prompt}`);
-		queue.push({ prompt: prompt, duration: duration, interaction: interaction });
+		queue.push({ prompt: prompt, params: params, interaction: interaction });
 		await interaction.reply({ content: `Your prompt \`${prompt}\` has been added to the queue. I'll notify you when it's ready!` });
 	}
 });
@@ -63,14 +75,19 @@ client.on('interactionCreate', async interaction => {
 async function processQueue() {
 	while (true) {
 		if (queue.length > 0) {
-			const { prompt, duration, interaction } = queue.shift();
+			const { prompt, params, interaction } = queue.shift();
 			const channel = interaction.channel;
 			const user = interaction.user;
 
 			try {
 				const response = await axios.post(apiEndpoint, {
 					prompt: prompt,
-					duration: duration
+					duration: params.duration,
+					temperature: params.temperature,
+					top_k: params.top_k,
+					top_p: params.top_p,
+					cfg_coef: params.cfg_coef,
+					extend_stride: params.extend_stride
 				}, { responseType: 'arraybuffer' });
 
 				const musicFile = response.data;
